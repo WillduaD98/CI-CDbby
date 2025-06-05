97e8a0bca79ea56b8ab4b4a5e7d83d7241399aed
[William Duarte] <[willi_dds3@hotmail.com]>
2025-06-04 18:43:22 -0600

    adding fixes

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -4,6 +4,20 @@ import cleanDB from "./cleanDb.js";
 
 import pythonQuestions from './pythonQuestions.json' with { type: "json" };
 
+export const seedQuestions = async () => {
+  try {
+    const count = await Question.countDocuments();
+    if (count ===0) {
+      await cleanDB('Question', 'questions');
+      await Question.insertMany(pythonQuestions);
+      console.log(`Seed Inserted`)
+    }
+  } catch (error) {
+    console.error('Error seeding questions:', error);
+    process.exit(1);
+  }
+}
+
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
 
